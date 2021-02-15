173f92c90b288bea6eba61ce31d0e6a206008737
giovanniscalzotto <gscalzot@stevens531884>
2021-02-14 20:17:27 -0500

    Assignment1

--- Assignment1.py
+++ Assignment1.py
@@ -14,9 +14,7 @@ plt.ylabel('Sin and Cos', fontsize = 12)
 plt.legend(['Sin', 'Cos'], loc = "upper right", frameon = False, fontsize = 10)
 plt.title('Assignment 1', loc='left', fontsize = 20)
 plt.show()
-a = 2
-b = 1
-print(a+b)
+
 
 
 
